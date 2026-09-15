63e869c9799e2d8bf9a37f4700a3008b2badca0e
123311-oss <michael.leandrodantas@gmail.com>
2026-09-15 12:18:04 -0300

    Simplify callback response for Mercado Livre

--- api/auth/callback.js
+++ api/auth/callback.js
@@ -1,22 +1,3 @@
-module.exports = (req, res) => { 
-  const { code, error } = req.query;
-
-  if (error) {
-    return res.status(400).json({
-      ok: false,
-      error: error
-    });
-  }
-
-  if (!code) {
-    return res.status(200).json({
-      ok: true,
-      message: "Callback do Mercado Livre ativo."
-    });
-  }
-
-  return res.status(200).json({
-    ok: true,
-    message: "Código do Mercado Livre recebido."
-  });
+module.exports = (req, res) => {
+  res.status(200).send("Callback do Mercado Livre funcionando!");
 };
